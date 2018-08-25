3ae660e8e260d122667868f539e4876e66f8cfb0
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2018-08-25 11:09:33 +0900

    Update Map.js
    Make setMarker function for making marker and connect with locations.json

--- src/Map.js
+++ src/Map.js
@@ -2,21 +2,40 @@ import React, { Component } from 'react';
 
 let changeArray = [];
 let map;
-//variables for infowindow
-let api;
-let data;
-let name;
-let prefix;
-let suffix;
-let address;
-let canonicalUrl;
-let largeInfowindow;
+
 //JSON data from ''./src/data'
 let parks = require('./data/locations.json').parks;
 let cafes = require('./data/locations.json').cafes;
 let restaurants = require('./data/locations.json').restaurants;
 
 class Map extends Component {
+  //Setting marker at switch loop.
+  setMarker(value) {
+    for (let i = 0; i < value.length; i++) {
+      let position = value[i].location;
+      let title = value[i].title;
+      let marker = new window.google.maps.Marker({
+        map: map,
+        position: position,
+        title: title,
+        animation: window.google.maps.Animation.DROP,
+        id: value[i].id
+      });
+      changeArray.push(marker);
+      this.props.inputMarkers(changeArray);
+      //onclick event to open infowindow
+      marker.addListener('click', () => {
+        this.props.getDetails(map, marker);
+        marker.setAnimation(null);
+      }, {passive: true});
+      marker.addListener('keypress', (e) => {
+        if(e.key === 'Enter') {
+          this.props.getDetails(map, marker);
+          marker.setAnimation(null);
+        }
+      })
+    }
+  }
 
   initMap() {
     console.log('init ' + this.props.filter);
@@ -25,142 +44,31 @@ class Map extends Component {
       zoom: 11,
       mapTypeControl: false
     });
-    largeInfowindow = new window.google.maps.InfoWindow();
+    this.props.storeMap(map);
     switch(this.props.filter) {
       case 'parks':
         changeArray = [];
-        console.log(parks.length)
-        for (let i = 0; i < parks.length; i++) {
-          let position = parks[i].location;
-          let title = parks[i].title;
-          let marker = new window.google.maps.Marker({
-            map: map,
-            position: position,
-            title: title,
-            animation: window.google.maps.Animation.DROP,
-            id: parks[i].id
-          });
-          changeArray.push(marker);
-          //onclick event to open infowindow
-          marker.addListener('click', () => {
-            this.getDetails(marker, largeInfowindow);
-            marker.setAnimation(null);
-          }, {passive: true});
-          marker.addListener('keypress', (e) => {
-            if(e.key === 'Enter') {
-              console.log('a')
-              this.getDetails(marker, largeInfowindow);
-              marker.setAnimation(null);
-            }
-          })
-        }
-        this.props.inputMarkers(changeArray);
+        this.setMarker(parks);
         break;
       case 'cafes':
         changeArray = [];
-        for (let i = 0; i < cafes.length; i++) {
-          let position = cafes[i].location;
-          let title = cafes[i].title;
-          let marker = new window.google.maps.Marker({
-            map: map,
-            position: position,
-            title: title,
-            animation: window.google.maps.Animation.DROP,
-            id: cafes[i].id
-          });
-          changeArray.push(marker);
-          this.props.inputMarkers(changeArray);
-          //onclick event to open infowindow
-          marker.addListener('click', () => {
-            this.getDetails(marker, largeInfowindow);
-            marker.setAnimation(null);
-          }, {passive: true});
-        }
+        this.setMarker(cafes);
         break;
       case 'restaurants':
         changeArray = [];
-        for (let i = 0; i < restaurants.length; i++) {
-          let position = restaurants[i].location;
-          let title = restaurants[i].title;
-          let marker = new window.google.maps.Marker({
-            map: map,
-            position: position,
-            title: title,
-            animation: window.google.maps.Animation.DROP,
-            id: restaurants[i].id
-          });
-          changeArray.push(marker);
-          this.props.inputMarkers(changeArray);
-          //onclick event to open infowindow
-          marker.addListener('click', () => {
-            this.getDetails(marker, largeInfowindow);
-            marker.setAnimation(null);
-          }, {passive: true});
-        }
+        this.setMarker(restaurants);
         break;
       case 'all' :
         changeArray = [];
-        for (let i = 0; i < parks.length; i++) {
-          let position = parks[i].location;
-          let title = parks[i].title;
-          let marker = new window.google.maps.Marker({
-            map: map,
-            position: position,
-            title: title,
-            animation: window.google.maps.Animation.DROP,
-            id: parks[i].id
-          });
-          changeArray.push(marker);
-          this.props.inputMarkers(changeArray);
-          //onclick event to open infowindow
-          marker.addListener('click', () => {
-            this.getDetails(marker, largeInfowindow);
-            marker.setAnimation(null);
-          }, {passive: true});
-        }
-        for (let i = 0; i < cafes.length; i++) {
-          let position = cafes[i].location;
-          let title = cafes[i].title;
-          let marker = new window.google.maps.Marker({
-            map: map,
-            position: position,
-            title: title,
-            animation: window.google.maps.Animation.DROP,
-            id: cafes[i].id
-          });
-          changeArray.push(marker);
-          this.props.inputMarkers(changeArray);
-          //onclick event to open infowindow
-          marker.addListener('click', () => {
-            this.getDetails(marker, largeInfowindow);
-            marker.setAnimation(null);
-          }, {passive: true});
-        }
-        for (let i = 0; i < restaurants.length; i++) {
-          let position = restaurants[i].location;
-          let title = restaurants[i].title;
-          let marker = new window.google.maps.Marker({
-            map: map,
-            position: position,
-            title: title,
-            animation: window.google.maps.Animation.DROP,
-            id: restaurants[i].id
-          });
-          changeArray.push(marker);
-          this.props.inputMarkers(changeArray);
-          //onclick event to open infowindow
-          marker.addListener('click', () => {
-            this.getDetails(marker, largeInfowindow);
-            marker.setAnimation(null);
-          }, {passive: true});
-        }
+        this.setMarker(parks);
+        this.setMarker(restaurants);
+        this.setMarker(cafes);
         break;
       default:
         console.log('Now there is no filter')
         break;
     }
   }
-  // AIzaSyBQzDYeihQyVFS3NhEU0pruH4DiKrKjrC0
   /* Refered by http://cuneyt.aliustaoglu.biz/en/using-google-maps-in-react-without-custom-libraries/ */
   componentDidMount() {
     if (!window.google) {
@@ -181,46 +89,6 @@ class Map extends Component {
     }
   }
 
-  //Create InfoWindow about marker
-  createInfoWindow(marker, infowindow) {
-    if(infowindow.marker !== marker) {
-      infowindow.marker = marker;
-
-      infowindow.addListener('closeclick', () => {
-        infowindow.marker = null;
-      });
-      infowindow.setContent(`<div>
-        <h2 tabIndex="0">${name}</h2>
-        <img tabIndex="0" src="${prefix}240x130${suffix}" alt="best photo of ${name}" />
-        <h3 tabIndex="0">Address : ${address}</h3>
-        <a href="${canonicalUrl}" target="_blank" tabIndex="0">Visit Foursquare page</a>
-        </div>`)
-      infowindow.open(map, marker);
-    }
-  }
-
-  //Get data of Foursquare and store them in variables EKTTGCNFBQA4PWPHF0T4OHBXMKBLMQEGCXEHDSVDCL4X2VF4
-  getDetails(marker, infowindow) {
-    api = `https://api.foursquare.com/v2/venues/${marker.id}?client_id=EKTTGCNFBQA4PWPHF0T4OHBXMKBLMQEGCXEHDSVDCL4X2VF4&client_secret=3WTGE0UZLTWFNZOQ1DNG3J1PC4DNRCS2PXEXJOGWKOI0C5WP&v=20180813`;
-    fetch(api, {
-      method: 'GET'
-    }).then(res => {
-      if(res.status === 200) {
-        data = res.json().then(data => {
-          name = data.response.venue.name;
-          address = data.response.venue.location.address;
-          canonicalUrl = data.response.venue.canonicalUrl;
-          prefix = data.response.venue.bestPhoto.prefix;
-          suffix = data.response.venue.bestPhoto.suffix;
-          console.log('fetch successed: address= ' + address + ' url=' + canonicalUrl);
-          //run createInfoWindow function
-          this.createInfoWindow(marker, infowindow);
-        })
-      } else {
-        alert('Foursquare error from fetch status. Check client_id or client_secret.' );
-      }
-    })
-  }
   render() {
     return (
       <div className="myMap" />
