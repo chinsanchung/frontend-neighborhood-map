a45d2eac81822bae65feca05838146d3f22c49af
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-03-30 22:46:03 +0900

    change: Move directory

---
+++ src/pages/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <nav className="navbar navbar-expand navbar-dark bg-primary header">
+            <span className="navbar-brand mx-auto">
+                Neigborhood Map with Google
+            </span>
+        </nav>
+        // <header className="header col-12 col-sm">
+        //     <div className="container">
+        //         <div className="row align-self-center">
+        //             <div className="col-12">
+        //                 Neigborhood Map with Google
+        //             </div>
+        //         </div>
+        //     </div>
+        // </header>
+    )
+}
+
+export default Header;
