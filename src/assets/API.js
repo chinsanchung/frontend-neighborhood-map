f135fd12267d19732ee0c9144694c785a21d06dd
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-03-30 22:44:58 +0900

    Update: add photo_link constant

--- src/assets/API.js
+++ src/assets/API.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const MAP_KEY = 'AIzaSyBxJyYXLJmaN_8UfUHsWgkg_kIZcCB6vqQ';
+export const PHOTO_LINK = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=90&maxheight=100&photoreference'
 
 export const getPlaceDetail = async () => {
     try {
@@ -9,3 +10,8 @@ export const getPlaceDetail = async () => {
         console.log('Error from Using Places API')
     }
 }
+
+// ex
+// `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.570148,126.976816&radius=1000&type=restaurant&key=${MAP_KEY}`
+// `https://maps.googleapis.com/maps/api/place/details/json?placeid=${place_id}&key=${MAP_KEY}`
+// `https://maps.googleapis.com/maps/api/place/photo?maxwidth=100&photoreference=${photo_reference}&key=${MAP_KEY}`
